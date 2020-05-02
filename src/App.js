133118ead8798a097ff9d103a39eb29a75ccb800
allison_johnson <allison.cuttler@gmail.com>
2020-05-02 15:29:25 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+    //debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -28,13 +28,21 @@ class App extends Component {
             Click to change user count
           </button>
           <p>{this.props.items.length}</p>
+          {/* <p>{this.props.users.length}</p> */}
       </div>
     );
   }
 }
 
+// const vanilla = (milkshake) => {
+//   debugger;
+//   return { items: milkshake.items }
+// }
+
+// export default connect(vanilla)(App);
+
 const mapStateToProps = (state) => {
-  debugger;
+  //debugger;
   return { items: state.items }
 }
 
